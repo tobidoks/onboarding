15e2cbd586444bbdfeed3937f56ab3b06162a96f
folaraz <folaraz11@gmail.com>
2022-06-02 17:55:26 +0100

    initial project setup

---
+++ projects/onboarding-service/src/test/java/com/olyves/onboarding/OnboardingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olyves.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
