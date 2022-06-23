f1b477b4792abdbdeed73785714f265bccefbee2
akanola <folarin@getolyves.com>
2022-06-24 00:10:15 +0100

    initial commit

--- projects/authentication-service/src/main/java/com/olyves/authentication/controller/PasswordController.java
+++ projects/authentication-service/src/main/java/com/olyves/authentication/controller/PasswordController.java
@@ -1,4 +1,12 @@
 package com.olyves.authentication.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/v1/password")
 public class PasswordController {
+
 }
